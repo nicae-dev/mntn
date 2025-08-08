b36ef08d4cec1490875e125585115a722cad8d29
Veronica Efimova <nica.e@yandex.ru>
2025-08-08 12:10:03 +0300

    мелкие изменения

--- source/scripts/smooth-scroll.js
+++ source/scripts/smooth-scroll.js
@@ -3,7 +3,6 @@ smoothLinks.forEach((smoothLink) => smoothLink.addEventListener('click', (e) => 
   e.preventDefault();
   const id = smoothLink.getAttribute('href');
   if (id) {
-
     document.querySelector(id).scrollIntoView({
       behavior: 'smooth',
       block: 'start'
